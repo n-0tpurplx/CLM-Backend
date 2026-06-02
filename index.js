6781787e952040771beef1add15ce3947bac9d2b
n0tpurplx_ <silvertandluccc@gmail.com>
2026-06-02 19:20:26 +0200

    Implement Discord login and callback routes
    Added authentication routes for Discord login and callback.

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Backend is alive");
+  res.send("Backend alive");
+});
+
+// STEP 1: login redirect (temporary stub)
+app.get("/auth/discord", (req, res) => {
+  res.send("Login route works (next we add Discord redirect)");
+});
+
+// STEP 2: callback (THIS FIXES YOUR ERROR)
+app.get("/auth/discord/callback", (req, res) => {
+  res.send("Callback reached successfully 🎉");
 });
 
 app.listen(3000, () => {
